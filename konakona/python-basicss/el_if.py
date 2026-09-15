3805a3613757ca1b00fe35609a310fea4b1e72b2
GiftyMerin <giftymerinjoy@gmail.com>
2026-09-15 15:49:33 +0530

    Add Python basics files

---
+++ konakona/python-basicss/el_if.py
@@ -0,0 +1,10 @@
+n = int(input("Enter the marks: "))
+
+if n>90:
+    print("Grade A")
+elif(n>70):
+    print("Grade B")
+elif(n>60):
+    print("Grade C")
+else:
+    print("Failed")
---
+++ konakona/python-basicss/new.py
@@ -0,0 +1,36 @@
+# a = "abhinand"
+# print(a)
+
+# list = [1,2,3,4,5,6]
+# print(list)
+
+# fruits = ["apple","orange","mango"]
+# print(fruits)
+
+# fruits.append("cherry")
+# print(fruits)
+
+# fruits.insert(1,"avacado")
+# print(fruits)
+
+# fruits.extend(["blueberry","pineapple","strawberry"])
+# print(fruits)
+
+
+tuple = (1,2,3,4,5,6)
+print(tuple)
+
+print(len(tuple))
+print(max(tuple))
+print(min(tuple))
+print(sum(tuple))
+
+dict = {
+    "name" : "amal",
+    "age" : 20,
+    "place" : "calicut"
+}
+print(dict)
+
+dict["age"]=25
+print(dict)
